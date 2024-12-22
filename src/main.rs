8a57d9ee4190022d4a7eff9f23f36737c1a82426
Eureka <1833055048@qq.com>
2024-12-22 22:09:38 +0800

    how to generate a random identifier

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ pub mod lia_logic;
 pub mod parser;
 pub mod z3_solver;
 pub mod enum_synth;
+pub mod collect_callings;
 fn main() {
     enum_synth::enum_synth_fun();
     println!("Hello, world!");
